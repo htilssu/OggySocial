f0e257d6c1ecdeb94361ca38329bca19f0df7753
htilssu <tolashuu@gmail.com>
2024-02-01 11:03:07 +0700

    add: main layout

--- app/src/main/java/com/oggysocial/oggysocial/activities/MainActivity.java
+++ app/src/main/java/com/oggysocial/oggysocial/activities/MainActivity.java
@@ -2,25 +2,41 @@ package com.oggysocial.oggysocial.activities;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.oggysocial.oggysocial.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    MaterialToolbar toolbar;
+    BottomNavigationView bottomNavigationView;
+
+    Fragment homeFragment, profileFragment, settingFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setSupportActionBar(findViewById(R.id.toolbar));
+        initVariables();
+    }
+
+    public void initVariables() {
+        toolbar = findViewById(R.id.toolbar);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+    }
+
+    public void initListener() {
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home_item) {
+            } else if (itemId == R.id.profile_item) {
+            } else if (itemId == R.id.setting_item) {
+            }
+            return true;
         });
     }
 }
