6b2153a8d059ca8c0a5cf711ec02a620a9025d8e
htilssu <tolashuu@gmail.com>
2024-03-23 12:50:10 +0700

    user layout

---
+++ app/src/main/java/com/oggysocial/oggysocial/fragments/admin/UserAdminFragment.java
@@ -0,0 +1,33 @@
+package com.oggysocial.oggysocial.fragments.admin;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.oggysocial.oggysocial.R;
+
+
+public class UserAdminFragment extends Fragment {
+
+
+    public UserAdminFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_user_admin, container, false);
+    }
+}
