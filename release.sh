1389493ce7e352a60b6b334038dc989d70c86900
htilssu <tolashuu@gmail.com>
2024-03-26 18:42:20 +0700

    update release workflow

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@ old_apk="app-release.apk"
 new_apk="oggysocial.apk"
 release_dir="app/build/outputs/apk/release/"
 
-old_apk_path=$release_dir+$old_apk
+old_apk_path=$release_dir$old_apk
 mv $old_apk_path $new_apk
